47dd9e52b3e1bc212c49019f49d6bb74e35771b7
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-04 06:53:31 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -11,6 +11,11 @@ export default class App extends Component {
         <Text style={styles.introduction}>Tamanho da grade:
           {params.getRowsAmount()}X{params.getColumnsAmount()}</Text>
         <Field /> 
+        <Field opened /> 
+        <Field opened nearMines={1} /> 
+        <Field opened nearMines={2} /> 
+        <Field opened nearMines={3} /> 
+        <Field opened nearMines={6} /> 
       </View>
     )
   }
