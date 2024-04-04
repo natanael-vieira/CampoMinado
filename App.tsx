546e83b67d5382aa7a51b78be26dc6dc74e3fc7f
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-04 06:25:28 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import params from './src/params'
 export default class App extends Component {
   render() {
     return (
-      <View style={StyleSheet.container}>
+      <View style={styles.container}>
         <Text style={styles.welcome}>Iniciando o Mines</Text>
         <Text style={styles.introduction}>Tamanho da grade:
           {params.getRowsAmount()}X{params.getColumnsAmount()}</Text>
@@ -13,3 +13,18 @@ export default class App extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fsfcff'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  introduction: {}
+})
