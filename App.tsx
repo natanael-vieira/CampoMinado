9f6638fc9bffed8b62341f307488e4462cbfa344
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-08 07:22:22 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default class App extends Component {
 
   constructor(props: {}) {
     super(props)
-    this.state = this.createState 
+    this.state = this.createState() 
   }
 
   minesAmount = () => {
