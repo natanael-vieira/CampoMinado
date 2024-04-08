034375d25c1d3a783077871012c4cf23c78ddf91
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-08 05:43:08 -0300

    Update functions.js

---
+++ src/functions.js
@@ -0,0 +1,15 @@
+const createBoard = (rows, columns) => {
+    return Array(rows).fill(0).map((_, row) => {
+        return Array(columns).fill(0).map((_, column) => {
+            return {
+                row,
+                column,
+                opened: false,
+                flagged: false,
+                mined: false,
+                exploded: false,
+                nearMines: 0
+            }
+        })
+    })
+}
