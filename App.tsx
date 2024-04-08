1c24d8097a1fbada99b4bc785eac110feff3dca9
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-08 06:15:27 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import params from './src/params'
 import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
 
 export default class App extends Component {
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
