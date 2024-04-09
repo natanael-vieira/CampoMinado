61b9349f96855b6994a8b3cfdebcfe0bd7bd44f6
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-04-09 06:03:06 -0300

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -92,6 +92,8 @@ const invertFlag = (board, row, column) => {
     field.flagged = !field.flagged
 }
 
+const flagsUsed = board => fields(board).filter(field => field.flagged).length
+
 export { 
     createMinedBoard, 
     cloneBoard,
@@ -99,5 +101,6 @@ export {
     hadExplosion,
     wonGame,
     showMines,
-    invertFlag
+    invertFlag,
+    flagsUsed
 }
